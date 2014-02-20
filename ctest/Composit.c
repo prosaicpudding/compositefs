3ee08ca218a51e9d57e7f4d33cffcfb94973d61f
prosaicpudding <hnc10@my.fsu.edu>
2014-02-20 16:26:46 -0500

    added test folders

---
+++ ctest/Composit.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main (void)
+{
+	printf("I am program a\n");
+	int i=0;
+	++i;
+	printf("Result: %d\n",i);
+	return 0;
+}
+#include <stdio.h>
+
+
+
+
+
+int main(void)
+{
+
+	
+
+
+
+
+
+	return 0;
+}
+#include <stdio.h>
+
+int main(void)
+{
+	int a,b,c;
+	b=1, c=10;
+	a=b+c;
+	printf("Program C says 1+10=%d\n",a);
+	return 0;
+}
